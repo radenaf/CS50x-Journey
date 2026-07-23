96b7d97792b701712ff03ecb896627efac29fed2
radenaf <radenaffiqazizi@gmail.com>
2026-07-23 10:43:38 +0800

    230726

---
+++ rvrs/reverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    string text = get_string( "Input: " );
+
+    for (int i = strlen(text) - 1; i >= 0; i--)
+    {
+        printf("%c", text[i]);
+    }
+printf("\n");
+
+}
